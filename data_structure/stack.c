345f9c697c501e5d449e25adae6e0d071da1a01a
takeruOtani <otanitakeru.sub@gmail.com>
2024-09-14 11:59:05 +0900

    スタックの追加

---
+++ data_structure/stack.c
@@ -0,0 +1,77 @@
+/*
+スタック
+*/
+
+#include <stdio.h>
+
+#include "print_array.h"
+
+static int MAX_SIZE = 10;
+
+int stack[10];
+
+int top = 0;
+
+void push(int x);
+int pop(void);
+int is_empty(void);
+int is_full(void);
+
+int main(void){
+    for(int i = 0; i < 10; i++){
+        push(i);
+    }
+
+    print_int_array1d(stack, 10);
+    puts("");
+
+    for(int i = 0; i < 5; i++){
+        pop();
+    }
+
+    print_int_array1d(stack, 10);
+    puts("");
+
+    for(int i = 0; i < 5; i++){
+        push(i);
+    }
+
+    print_int_array1d(stack, 10);
+    puts("");
+
+    for(int i = 0; i < 10; i++){
+        pop();
+    }
+
+    return 0;
+}
+
+void push(int x){
+    if(is_full()){
+        printf("stack is full\n");
+        return;
+    }
+    stack[top] = x;
+    top++;
+}
+
+int pop(void){
+    if(is_empty()){
+        printf("stack is empty\n");
+        return -1;
+    }
+    top--;
+    int return_value = stack[top];
+    stack[top] = 0;
+    return return_value;
+}
+
+int is_empty(void){
+    return top == 0;
+}
+
+int is_full(void){
+    return top == MAX_SIZE;
+}
+
+
